2630b79b9824c44616368d3308a32ccd4e8a963a
Adinkra Ideas <92604245+Adinkra-Ideas@users.noreply.github.com>
2024-10-01 15:39:58 +0200

    Update HeadUnitHMI.cpp

--- src/HeadUnitHMI.cpp
+++ src/HeadUnitHMI.cpp
@@ -8,6 +8,8 @@ HeadUnitHMI::HeadUnitHMI(GearHMI & gearHMI, QObject *parent) :
     connect(this, &HeadUnitHMI::operate, &_gearHMI, &GearHMI::doWork);
     // signals will be emitted from this in order to connect() with a method in thread
     // more connect() can be added here depending on your needs
+    // Now we need to work on RPi to use the c lib grom gearHMI to manipulate speed
+    // Then we can now look into putting the construct of this that calls gearHMI into the thread by manipulating the object called from main()
     theThread.start();
 }
 
